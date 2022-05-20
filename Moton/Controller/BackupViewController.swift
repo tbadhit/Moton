0f6bc4a805a9cebe4458cc8798d5d20a542c5600
Fritzie Primananda Adi Praja <primanandafritzie@gmail.com>
2022-05-20 10:08:35 +0800

    fungsi bawaan project calendar

--- Moton/Controller/BackupViewController.swift
+++ Moton/Controller/BackupViewController.swift
@@ -109,4 +109,49 @@
 //    return calendarKitEvents
 //}
 
+//fungsi bawaan priject Calendar.swift
+//override func dayViewDidSelectEventView(_ eventView: EventView) {
+//    guard let ckEvent = eventView.descriptor as? EKWrapper else {
+//        return
+//    }
+//    presentDetailViewForEvent(ckEvent.ekEvent)
+//}
+//
+//private func presentDetailViewForEvent(_ ekEvent: EKEvent) {
+//    let eventController = EKEventViewController()
+//    eventController.event = ekEvent
+//    eventController.allowsCalendarPreview = true
+//    eventController.allowsEditing = false
+//    navigationController?.pushViewController(eventController,
+//                                             animated: true)
+//}
+//
+//// MARK: Event Editing
+//
+//override func dayView(dayView: DayView, didLongPressTimelineAt date: Date) {
+//    // Cancel editing current event and start creating a new one
+//    endEventEditing()
+//    let newEKWrapper = createNewEvent(at: date)
+//    create(event: newEKWrapper, animated: true)
+//}
+//
+//private func createNewEvent(at date: Date) -> EKWrapper {
+//    let newEKEvent = EKEvent(eventStore: eventStore)
+//    newEKEvent.calendar = eventStore.defaultCalendarForNewEvents
+//
+//    var components = DateComponents()
+//    components.hour = 1
+//    let endDate = calendar.date(byAdding: components, to: date)
+//
+//    newEKEvent.startDate = date
+//    newEKEvent.endDate = endDate
+//    newEKEvent.title = "New event"
+//
+//    let newEKWrapper = EKWrapper(eventKitEvent: newEKEvent)
+//    newEKWrapper.editedEvent = newEKWrapper
+//    return newEKWrapper
+//}
+
+//
+
 //}
